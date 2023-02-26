bbb8ade459132322a8946ec65e28549d2e95b13d
Saurabh9693 <98611231+Saurabh9693@users.noreply.github.com>
2023-02-27 00:25:11 +0530

    Add files via Upload

---
+++ index.js
@@ -0,0 +1,50 @@
+// for(var i=0; i<=6; i++){
+//     document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+// }
+
+
+// function handleClick(){
+//     alert("I got clicked");
+// }
+
+var x = document.getElementById("myaudio1");
+
+function playaudio1(){
+    x.play();
+}
+
+var y = document.getElementById("myaudio2");
+
+function playaudio2(){
+    y.play();
+}
+
+var z = document.getElementById("myaudio3");
+
+function playaudio3(){
+    z.play();
+}
+
+var a = document.getElementById("myaudio4");
+
+function playaudio4(){
+    a.play();
+}
+
+var b = document.getElementById("myaudio5");
+
+function playaudio5(){
+    b.play();
+}
+
+var c = document.getElementById("myaudio6");
+
+function playaudio6(){
+    c.play();
+}
+
+var d = document.getElementById("myaudio7");
+
+function playaudio7(){
+    d.play();
+}
